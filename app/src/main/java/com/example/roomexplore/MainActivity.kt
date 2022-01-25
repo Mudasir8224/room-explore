a6b4d2a97e34c59802ecfae80a7edf3339a921c0
Mr Mudasir <mudasir8224@gmail.com>
2022-01-25 11:14:00 +0500

    add layout

--- app/src/main/java/com/example/roomexplore/MainActivity.kt
+++ app/src/main/java/com/example/roomexplore/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.roomexplore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView = findViewById(R.id.rv)
+        setAdapter()
     }
+
+    fun setAdapter() {
+        val wordAdapter = WordAdapter()
+        recyclerView.adapter = wordAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+
 }
---
+++ app/src/main/java/com/example/roomexplore/MyApplication.kt
@@ -0,0 +1,4 @@
+package com.example.roomexplore
+
+class MyApplication: {
+}
